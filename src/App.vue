369d3f5c387d0e1784b0ebab8b8d4b37fb95d5be
Valentin Ichkour <valentin.ichkour@epitech.eu>
2020-07-15 18:31:15 +0200

    init repo

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <img src="@/assets/nonogram.png" alt style="width: 100px;" />
+      <h1>Nonogram solver based on images</h1>
+      <b-field class="file">
+        <b-upload v-model="file" expanded>
+          <a class="button is-primary is-fullwidth">
+            <b-icon icon="upload"></b-icon>
+            <span v-if="file">{{ file.name }}</span>
+            <span v-else>Click to upload</span>
+          </a>
+        </b-upload>
+      </b-field>
+      <b-button :disabled="!file" type="is-primary">Solve !</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      file: null,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

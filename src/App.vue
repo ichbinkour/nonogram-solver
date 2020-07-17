c71b3a9451b01c018a01a6fa0dc308ce7095a9ef
Valentin Ichkour <valentin.ichkour@epitech.eu>
2020-07-17 18:29:14 +0200

    blocks

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,55 @@
   <div id="app">
     <div class="container">
       <img src="@/assets/nonogram.png" alt style="width: 100px;" />
-      <h1>Nonogram solver based on images</h1>
-      <b-field class="file">
-        <b-upload v-model="file" expanded>
+      <h1>Nonogram solver</h1>
+
+      <div class="center">
+        <b-field label="top">
+          <b-numberinput
+            v-model="top"
+            controls-position="compact"
+          ></b-numberinput>
+        </b-field>
+        <b-field label="left">
+          <b-numberinput
+            v-model="left"
+            controls-position="compact"
+          ></b-numberinput>
+        </b-field>
+      </div>
+
+      <!-- <b-field class="file">
+        <b-upload v-model="file" expanded class="uploader">
           <a class="button is-primary is-fullwidth">
             <b-icon icon="upload"></b-icon>
             <span v-if="file">{{ file.name }}</span>
             <span v-else>Click to upload</span>
           </a>
         </b-upload>
-      </b-field>
+      </b-field> -->
+      <nonogram-grid :top="top" :left="left"></nonogram-grid>
       <b-button :disabled="!file" type="is-primary">Solve !</b-button>
+    </div>
+
+    <div>
+      <div v-if="file">{{ file }}</div>
     </div>
   </div>
 </template>
 
 <script>
+import NonogramGrid from "@/components/NonogramGrid.vue";
+
 export default {
   name: "app",
+  components: {
+    NonogramGrid,
+  },
   data() {
     return {
       file: null,
+      top: 3,
+      left: 3,
     };
   },
 };
@@ -35,5 +63,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.center {
+  margin: auto;
+  width: 20%;
+  padding: 10px;
 }
 </style>
---
+++ src/components/NonogramGrid.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <p>{{ top }} x {{ left }}</p>
+    <div class="ng-container">
+      <div
+        v-for="t in top"
+        :key="t"
+        style="margin-bottom: 1em;"
+        class="ng-line"
+      >
+        <div v-for="l in left" :key="l">
+          <div v-for="ca in 5" :key="ca" style="margin-bottom: -8px;">
+            <div v-for="cb in 5" :key="cb" class="ng-cell"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NonogramGrid",
+  props: {
+    top: {
+      type: Number,
+      default: 3,
+    },
+    left: {
+      type: Number,
+      default: 3,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.ng-line {
+  width: 100%;
+  text-align: center; // center the content of the container
+}
+.ng-container {
+  border: 3px solid green;
+}
+.ng-cell {
+  border: 1px solid red;
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+}
+</style>
